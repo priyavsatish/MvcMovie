431b2fa40f03f8746c3928d75a0b14ebcae6124c
Priya Satish <priyavelan16@outlook.com>
2016-11-14 11:36:30 -0500

    First MvcMovie Commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        // 
+        // GET: /HelloWorld/ 
+        public class RouteConfig
+        {
+            public static void RegisterRoutes(RouteCollection routes)
+            {
+                routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+                routes.MapRoute(
+                    name: "Default",
+                    url: "{controller}/{action}/{id}",
+                    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                );
+
+                routes.MapRoute(
+                     name: "Hello",
+                     url: "{controller}/{action}/{name}/{id}"
+                 );
+            }
+        }
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome(string Priya, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + Priya + ", ID: " + ID);
+        }
+    }
+}
